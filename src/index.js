a672143d5a862e42777b1f381a7a8ae23aebfd9d
NCSA Mosaic <1239085033@qq.com>
2021-11-14 14:45:20 +0800

    #feat 创建一个bundle

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-function component() {
-  const element = document.createElement('div');
+import _ from 'lodash';
 
-  // lodash（目前通过一个 script 引入）对于执行这一行是必需的
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+ function component() {
+   const element = document.createElement('div');
 
-  return element;
-}
+  // lodash 在当前 script 中使用 import 引入
+   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-document.body.appendChild(component());
+   return element;
+ }
+
+ document.body.appendChild(component());
